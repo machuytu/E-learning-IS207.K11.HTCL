0ab40a9488db2b78846052cfd106a05c310b1120
Nhựt <dinhpro2014@gmail.com>
2019-11-17 22:45:58 +0700

    Trang Chu
    Tạo trang chủ mới

---
+++ E-learning-IS207.K11.HTCL/resources/views/HomePage/partials/header.blade.php
@@ -0,0 +1,86 @@
+<header id="site-header" class="header">
+  <div id="header-wrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <!-- Navbar -->
+          <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand logo" href="index.html">
+              <img id="logo-img" class="img-center" src="images/logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> <span></span>
+              <span></span>
+              <span></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <!-- Left nav -->
+              <ul id="main-menu" class="nav navbar-nav ml-auto mr-auto">
+                <li class="nav-item"> <a class="nav-link active" href="#ngoaingu">Ngoại ngữ</a>
+                  <ul>
+                    <li><a href="index.html">Tiếng Anh</a>
+                    </li>
+                    <li><a href="index-2.html">Tiếng Nhật</a>
+                    </li>
+                    <li><a href="index-3.html">Tiếng Hàn</a>
+                    </li>
+                    <li><a href="index-4.html">Tiếng Nga</a>
+                    </li>
+                    <li><a href="index-5.html">Tiếng Trung</a>
+                    </li>
+                    <li><a href="index-6.html">Tiếng Pháp</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item"> <a class="nav-link" href="#laptrinh">Lập trình</a>
+                  <ul>
+                    <li><a href="team.html">Phát triển ứng dụng Web</a>
+                    </li>
+                    <li><a href="team-single.html">Java</a>
+                    </li>
+                    <li><a href="faq.html">Python</a>
+                    </li>
+                    <li><a href="#">SQL</a>
+                      <ul>
+                        <li><a href="shortcode-accordions.html">SQL Server</a>
+                        </li>
+                        <li><a href="shortcode-blog-post.html">Oracle</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li><a href="coming-soon.html">C++/C#</a>
+                    </li>
+                    
+                  </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="#kynangsong">Kỹ năng sống</a>
+                  <ul>
+                    <li><a href="about-us.html">Tạo slide ấn tượng</a>
+                    </li>
+                    <li><a href="about-us-2.html">Kỹ năng giao tiếp thu hút</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Đồ Họa</a>
+                  <ul>
+                    <li><a href="services.html">Photoshop</a>
+                    </li>
+                    <li><a href="services-2.html">AI</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Blogs</a>
+                 
+                </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Liên hệ với chúng tôi</a>
+                
+                </li>
+              </ul>
+            </div>
+            <a class="btn buttonnhutcst3" href="loginlophoc" data-text="Đăng nhập"> <span>Đ</span><span>ă</span><span>n</span><span>g </span><span></span><span>N</span><span>h</span><span>ậ</span><span>p</span>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
