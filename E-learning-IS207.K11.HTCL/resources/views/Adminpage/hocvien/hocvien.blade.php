e2e72c3d3cb97edec51908ecabb6742ba42179e1
tu <machuytu@gmail.com>
2019-11-17 20:59:10 +0700

    edit html adminpage
    cut js file to partials

--- E-learning-IS207.K11.HTCL/resources/views/Adminpage/hocvien/hocvien.blade.php
+++ E-learning-IS207.K11.HTCL/resources/views/Adminpage/hocvien/hocvien.blade.php
@@ -85,44 +85,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
             <!--//footer-->
         </div>
 
-        <!-- side nav js -->
-        <script src='{{ asset('js/SidebarNav.min.js') }}' type='text/javascript'></script>
-        <script>
-            $('.sidebar-menu').SidebarNav()
-        </script>
-        <!-- //side nav js -->
-
-        <!-- Classie -->
-        <!-- for toggle left push menu script -->
-        <script src="{{ asset('js/classie.js') }}"></script>
-        <script>
-            var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
-				showLeftPush = document.getElementById( 'showLeftPush' ),
-				body = document.body;
-
-			showLeftPush.onclick = function() {
-				classie.toggle( this, 'active' );
-				classie.toggle( body, 'cbp-spmenu-push-toright' );
-				classie.toggle( menuLeft, 'cbp-spmenu-open' );
-				disableOther( 'showLeftPush' );
-			};
-
-			function disableOther( button ) {
-				if( button !== 'showLeftPush' ) {
-					classie.toggle( showLeftPush, 'disabled' );
-				}
-			}
-        </script>
-        <!-- //Classie -->
-        <!-- //for toggle left push menu script -->
-
-        <!--scrolling js-->
-        <script src="{{ asset('js/jquery.nicescroll.js') }}"></script>
-        <script src="{{ asset('js/scripts.js') }}"></script>
-        <!--//scrolling js-->
-
-        <!-- Bootstrap Core JavaScript -->
-        <script src="{{ asset('js/bootstrap.js') }}"> </script>
+        {{-- start js --}}
+        @include('adminpage.partials.js_adminpage')
+        {{-- end js --}}
 
 </body>
 
