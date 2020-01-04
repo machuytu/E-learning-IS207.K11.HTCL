815ffbbf04e10f88e1c6cffd3c9c89c6209df2b0
Nhựt <dinhpro2014@gmail.com>
2020-01-04 21:48:31 +0700

    finish_dashboard1_phienbancuoicung

--- E-learning-IS207.K11.HTCL/resources/views/HomePage/partials/headerdashboard.blade.php
+++ E-learning-IS207.K11.HTCL/resources/views/HomePage/partials/headerdashboard.blade.php
@@ -5,79 +5,21 @@
         <div class="col-lg-12">
           <!-- Navbar -->
           <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand logo" href="/">
+            <a class="navbar-brand logo" href="#">
               <img id="logo-img" class="img-center" src="{{asset('images/logo.png')}}" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> <span></span>
               <span></span>
               <span></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-              <!-- Left nav -->
-              <ul id="main-menu" class="nav navbar-nav ml-auto mr-auto font-nav">
-                <li class="nav-item"> <a class="nav-link active" href="#ngoaingu">Ngoại ngữ</a>
-                  <ul>
-                    <li><a href="index.html">Tiếng Anh</a>
-                    </li>
-                    <li><a href="index-2.html">Tiếng Nhật</a>
-                    </li>
-
-                  </ul>
-                </li>
-                <li class="nav-item"> <a class="nav-link" href="#laptrinh">Lập trình</a>
-                  <ul>
-                    <li><a href="team.html">Phát triển ứng dụng Web</a>
-                    </li>
-                    <li><a href="team-single.html">Java</a>
-                    </li>
-                    <li><a href="faq.html">Python</a>
-                    </li>
-                    <li><a href="#">SQL</a>
-                      <ul>
-                        <li><a href="shortcode-accordions.html">SQL Server</a>
-                        </li>
-                        <li><a href="shortcode-blog-post.html">Oracle</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li><a href="coming-soon.html">C++/C#</a>
-                    </li>
-
-                  </ul>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="#kynangsong">Kỹ năng sống</a>
-                  <ul>
-                    <li><a href="about-us.html">Tạo slide ấn tượng</a>
-                    </li>
-                    <li><a href="about-us-2.html">Kỹ năng giao tiếp thu hút</a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Đồ Họa</a>
-                  <ul>
-                    <li><a href="services.html">Photoshop</a>
-                    </li>
-                    <li><a href="services-2.html">AI</a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Blogs</a>
-
-                </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Liên hệ với chúng tôi</a>
-
-                </li>
-
-              </ul>
-            </div>
-            <div class="dropdown">
+          
   <button type="button" class="btncustom btn btn-primary dropdown-toggle" data-toggle="dropdown">
     Nguyễn Mạnh Hiếu
     <img src="https://courses.uit.edu.vn/pluginfile.php?file=%2F72575%2Fuser%2Ficon%2Fboost%2Ff2&amp;rev=1490024" class="userpicture1" width="35" height="35" role="presentation" aria-hidden="true " border-radius:20px;>
   </button>
-  <div class="dropdown-menu2 dropdown-menu ">
+  <div class="dropdown-menu2 dropdown-menu">
     @if (Auth::check())
-        <a href="{{ route('logout') }}">Logout</a>
+        <a href="{{ route('logout') }}">Đăng xuất</a>
     @endif
   </div>
 </div>
